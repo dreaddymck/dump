1ad0ae5c4ee4fac7daa8a0d55eedff7a36719ca3
dreaddymck <dreaddymck@users.noreply.github.com>
2020-09-28 12:20:16 -0500

    Create google-drive-file-downloader.sh
    Bash script that can be used to download files from Google Drive

---
+++ google-drive-file-downloader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ];
+then
+
+    echo "required arguments - 'File ID' 'Filename'"
+
+else
+
+    CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+    wget --limit-rate 1024K --continue --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+    rm -rf /tmp/cookies.txt
+
+fi
+
+exit 0
